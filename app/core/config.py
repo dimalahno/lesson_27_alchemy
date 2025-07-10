9a9242534f3d87384b88c6edb65be6c637bbeadd
dimalahno <dimalahno@rambler.ru>
2025-07-10 15:06:00 +0500

    Initial commit

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic.v1 import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    @property
+    def async_database_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+    class Config:
+        env_file =".env"
+
+
+settings = Settings()
